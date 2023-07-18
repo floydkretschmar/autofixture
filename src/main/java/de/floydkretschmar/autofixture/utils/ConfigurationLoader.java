7a0f9468642f33108e82f55ef069a0f496824a5f
Floyd Kretschmar <fkretschmar@googlemail.com>
2023-07-19 01:55:09 +0200

    Refactor using streaming api

--- src/main/java/de/floydkretschmar/autofixture/utils/ConfigurationLoader.java
+++ src/main/java/de/floydkretschmar/autofixture/utils/ConfigurationLoader.java
@@ -1,73 +1,52 @@
 package de.floydkretschmar.autofixture.utils;
 
-import lombok.Builder;
-import lombok.Value;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public final class ConfigurationLoader {
-
-    @Value
-    @Builder
-    static class LineData {
-        int whitespaces;
-        LineDataType type;
-    }
-
-    static enum LineDataType {
-        LIST_ITEM,
-        OBJECT
-    }
-
-    private static final Pattern includeListItemPattern = Pattern.compile("^(?<whitespaces> *)((?<listPrefix>- )|(?<objectPrefix>(.*: )))!include (?<fileName>.*\\.(yaml|yml)).*");
+    private static final Pattern includePattern = Pattern.compile("^(?<whitespaces> *)((?<listPrefix>- )|(?<objectPrefix>(.*: )))!include (?<fileName>.*\\.(yaml|yml)).*");
 
     public static String readConfiguration(String configurationFile) {
-        return readConfigurationAsList(configurationFile).stream().reduce("", (base, latest) -> base.isBlank() ? latest : "%s\r\n%s".formatted(base, latest));
+        return readConfigurationAsList(configurationFile).reduce("", (base, latest) -> base.isBlank() ? latest : "%s\r\n%s".formatted(base, latest));
     }
 
-    private static List<String> readConfigurationAsList(String configurationFile) {
+    private static Stream<String> readConfigurationAsList(String configurationFile) {
         final var configurationFileUrl = ConfigurationLoader.class.getClassLoader().getResource(configurationFile);
         if (configurationFileUrl == null)
             throw new ConfigurationReadException(configurationFile);
         try {
             final var linesOfOriginalConfiguration = Files.readAllLines(Path.of(configurationFileUrl.toURI()));
-            final var linesOfComposedConfiguration = new ArrayList<String>();
-            for (int i = 0; i < linesOfOriginalConfiguration.size(); i++) {
-                final var line = linesOfOriginalConfiguration.get(i);
-                var matcher = includeListItemPattern.matcher(line);
-                if (matcher.matches()) {
-                    final var includedFileName = matcher.group("fileName");
-                    final var type = Objects.nonNull(matcher.group("listPrefix")) ? LineDataType.LIST_ITEM : LineDataType.OBJECT;
 
-                    final var linesOfIncludedConfiguration = readConfigurationAsList(includedFileName);
-                    if (type == LineDataType.OBJECT) {
-                        final var whitespacesBeforeObjectName = matcher.group("whitespaces").length();
-                        linesOfComposedConfiguration.add("%s%s".formatted(" ".repeat(whitespacesBeforeObjectName), matcher.group("objectPrefix").stripTrailing()));
-                        linesOfIncludedConfiguration.forEach(includedConfigurationLine -> linesOfComposedConfiguration.add("%s%s".formatted(" ".repeat(whitespacesBeforeObjectName + 2), includedConfigurationLine)));
-                    } else {
-                        final var whitespacesBeforeHyphen = matcher.group("whitespaces").length();
-                        final var whitespacesBeforeItemContent = whitespacesBeforeHyphen + 2;
-                        linesOfComposedConfiguration.add("%s- %s".formatted(" ".repeat(whitespacesBeforeHyphen), linesOfIncludedConfiguration.get(0)));
-                        linesOfIncludedConfiguration.subList(1, linesOfIncludedConfiguration.size()).forEach(includedConfigurationLine -> linesOfComposedConfiguration.add("%s%s".formatted(" ".repeat(whitespacesBeforeItemContent), includedConfigurationLine)));
-                    }
-                } else {
-                    linesOfComposedConfiguration.add(line);
-                }
-            }
-            return linesOfComposedConfiguration;
+            return linesOfOriginalConfiguration.stream().flatMap(line -> {
+                var matcher = includePattern.matcher(line);
+                return matcher.matches() ? loadAndProcessSubConfiguration(matcher) : Stream.of(line);
+            });
         } catch (IOException | OutOfMemoryError | SecurityException | IllegalArgumentException |
                  FileSystemNotFoundException | URISyntaxException e) {
-            throw new ConfigurationReadException(configurationFile.toString(), e);
+            throw new ConfigurationReadException(configurationFile, e);
         }
     }
 
-
+    private static Stream<String> loadAndProcessSubConfiguration(Matcher matcher) {
+        final var includedFileName = matcher.group("fileName");
+        final var linesOfIncludedConfiguration = readConfigurationAsList(includedFileName);
+        if (Objects.nonNull(matcher.group("objectPrefix"))) {
+            final var whitespacesBeforeObjectName = matcher.group("whitespaces").length();
+            return Stream.concat(Stream.of("%s%s".formatted(" ".repeat(whitespacesBeforeObjectName), matcher.group("objectPrefix").stripTrailing())),
+                    linesOfIncludedConfiguration.map(includedConfigurationLine -> "%s%s".formatted(" ".repeat(whitespacesBeforeObjectName + 2), includedConfigurationLine)));
+        } else {
+            final var whitespacesBeforeHyphen = matcher.group("whitespaces").length();
+            final var whitespacesBeforeItemContent = whitespacesBeforeHyphen + 2;
+            final var linesAsList = linesOfIncludedConfiguration.toList();
+            return Stream.concat(Stream.of("%s- %s".formatted(" ".repeat(whitespacesBeforeHyphen), linesAsList.get(0))),
+                    linesAsList.stream().skip(1).map(includedConfigurationLine -> "%s%s".formatted(" ".repeat(whitespacesBeforeItemContent), includedConfigurationLine)));
+        }
+    }
 }
